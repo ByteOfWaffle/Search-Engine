13ab5071ff7860ac43df678e6bf5f4c8c8c92577
ByteOfWaffle <156892485+ByteOfWaffle@users.noreply.github.com>
2024-11-18 09:37:14 +0100

    Finished list and list split

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ for line in r: # Loop through each line in the file
     if search in line: #If the inputted word (in search) is in the line print the line
         print(line)
         list = [line]
-        print(list)
+        splitlist = line.split() #Splits every word in the line into a list item.
+        print(splitlist)
