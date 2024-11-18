077561156d5af89e0d10e071a93329f71d9f5ca4
ByteOfWaffle <156892485+ByteOfWaffle@users.noreply.github.com>
2024-11-18 09:26:33 +0100

    Text search

---
+++ main.py
@@ -0,0 +1,7 @@
+r = open("test.txt")
+search = input("Enter a word to search for: ")
+
+for line in r: # Loop through each line in the file
+    if search in line: #If the inputted word (in search) is in the line print the line
+        print(line)
+
