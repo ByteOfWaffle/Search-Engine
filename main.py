e413037e5a0f6737898358e1600da252d9ddf82e
ByteOfWaffle <156892485+ByteOfWaffle@users.noreply.github.com>
2024-11-18 09:31:24 +0100

    Added list?

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ search = input("Enter a word to search for: ")
 for line in r: # Loop through each line in the file
     if search in line: #If the inputted word (in search) is in the line print the line
         print(line)
-
+        list = [line]
+        print(list)
